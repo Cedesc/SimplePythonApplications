f34390b0d3f28aedc63ecd3cb8cefec935a5bd6c
Christian <irgendwas@nix.de>
2021-12-02 13:54:04 +0100

    feat: refactoring unfoldZip.py

--- hackcember/unfoldZip.py
+++ hackcember/unfoldZip.py
@@ -1,73 +1,52 @@
 from zipfile import ZipFile, is_zipfile
 import time
 
-file_name = "geschenk.zip"
 
-for i in range(1):
-    with ZipFile(file_name) as zip:
-        zip.printdir()
-        print(zip.namelist())
-        # zip.extractall()
-        with zip.open(zip.namelist()[0]) as innerZip:
-            zip.printdir()
-            print(innerZip)
-            z = ZipFile(innerZip)
-            z.printdir()
-            print(is_zipfile(innerZip))
+def unzip(input_zip: ZipFile, zip_save_folder_name: str = 'temp', result_save_folder_name: str = 'temp'):
+    """
+    Functionality: Unzip zipped zips
+    Requirement: Each ZIP contains only one file
+    Improvement suggestion:
+        Delete temporary saved zips after another zip was saved and save zips after less than 200 iterations.
+    """
 
+    startTimer = time.time()
+    timer = startTimer
 
-def unzip(input_zip: ZipFile):
-
-    starttimer = time.time()
-    timer = starttimer
-
+    # to prevent that the loop won't end, the maximum is set on 2500 (geschenk.zip has 2021 inner zips)
     for i in range(1, 2500):
 
+        # after every 50 iteration: show intermediate status
         if i % 50 == 0:
             print("Counter: ", i, "\n Time: ", time.time() - timer)
-            starttime = time.time()
+            timer = time.time()
 
         innerFile = input_zip.open(input_zip.namelist()[0])
 
+        # check if the inner file is a zip file or not
         if is_zipfile(innerFile):
 
-            # after every 200th iteration, save the current zip and use this further to speed up the operations
+            # after every 200th iteration: save current zip and use this saved zip for further operations to speed up
             if i % 200 == 0:
-                input_zip.extractall('temp')
+                input_zip.extractall(zip_save_folder_name)
+                # save the path of the saved zip for further operation
                 name = 'temp/' + input_zip.namelist()[0]
                 input_zip.close()
                 input_zip = ZipFile(name)
-                print("Zip Name: ", name)
+                print("  Current saved zip: ", name)
             else:
                 input_zip.close()
                 input_zip = ZipFile(innerFile)
         else:
-            print("\n\nAB HIER")
+            # print the last zip folder and the total time that the function has taken
+            print("\n\nLAST ZIP FOLDER")
             input_zip.printdir()
-            print("Namelist: ", input_zip.namelist())
-            print("Time: ", time.time() - starttimer)
-            print("Text of the .txt file: ", innerFile.read())
+            print(" Total Time: ", time.time() - startTimer)
+            # extract the inner file
+            input_zip.extractall(result_save_folder_name)
             break
 
 
-
-def rec_unzip(input_zip: ZipFile, counter=0):
-
-    if counter % 50 == 0:
-        print("Counter: ", counter, "\n Time: ", time.time())
-
-
-    with input_zip.open(input_zip.namelist()[0]) as innerZip:
-        if is_zipfile(innerZip):
-            rec_unzip(ZipFile(innerZip), counter + 1)
-        else:
-            print("\n\nAB HIER")
-            input_zip.printdir()
-            print("Namelist: ", input_zip.namelist())
-
-
-# print(rec_unzip(ZipFile(file_name)))
-# print(rec_unzip(ZipFile('test3.zip')))
-
-# unzip(ZipFile('test3.zip'))
-unzip(ZipFile(file_name))
+if __name__ == '__main__':
+    # unzip(ZipFile('test3.zip'))
+    unzip(ZipFile('geschenk.zip'))
