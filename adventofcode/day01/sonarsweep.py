7f36e5565930aa0532dad9dffd68bce9cff99a97
Christian <irgendwas@nix.de>
2021-12-05 10:17:57 +0100

    feat: added second solution to day01

--- adventofcode/day01/sonarsweep.py
+++ adventofcode/day01/sonarsweep.py
@@ -1,6 +1,6 @@
 
 
-def count_measurement_increases(measurement_text_file: str):
+def count_measurement_increases(measurement_text_file: str) -> int:
 
     counter = 0
 
@@ -19,5 +19,35 @@ def count_measurement_increases(measurement_text_file: str):
     return counter
 
 
+def count_sum_of_3_measurement_increases(measurement_text_file: str) -> int:
+
+    counter = 0
+
+    with open(measurement_text_file, "r") as file:
+        lines = file.read().split("\n")
+
+    second_predecessor_number: int = int(lines[0])
+    first_predecessor_number: int = int(lines[1])
+    actual_number: int = int(lines[2])
+
+    previous_sum: int = second_predecessor_number + first_predecessor_number + actual_number
+    actual_sum: int
+
+    for line in lines[3::]:
+        second_predecessor_number = first_predecessor_number
+        first_predecessor_number = actual_number
+        actual_number = int(line)
+
+        actual_sum = second_predecessor_number + first_predecessor_number + actual_number
+
+        if previous_sum < actual_sum:
+            counter += 1
+
+        previous_sum = actual_sum
+
+    return counter
+
+
 if __name__ == '__main__':
     print(count_measurement_increases('measurements.txt'))
+    print(count_sum_of_3_measurement_increases('measurements.txt'))
