ebf00c79ac4bae6c0cfc3e66948fe48929d5a30a
Christian <irgendwas@nix.de>
2022-01-24 13:02:43 +0100

    feat: added while loop to SecurePassword.py for using without IDE

--- PasswordManager/SecurePassword.py
+++ PasswordManager/SecurePassword.py
@@ -23,4 +23,12 @@ def createSecurePassword(password_length=40):
 
 
 if __name__ == '__main__':
+
     createSecurePassword(password_length=40)
+
+    while True:
+        length = input('\nGib "exit" oder nichts ein um das Programm zu beenden\n'
+              'Gib eine Zahl x ein um ein Passwort der Länge x zu erstellen\n')
+        if length == "" or length == "exit":
+            break
+        createSecurePassword(password_length=int(length))
