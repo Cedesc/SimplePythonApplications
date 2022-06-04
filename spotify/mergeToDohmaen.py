a45a2fe6fb6f908fe7f4d477aaa4b38768cfa3a4
Christian <irgendwas@nix.de>
2022-06-04 13:47:30 +0200

    feat: outsourced functions from mergeToDohmaen.py to spotifyAPIFunctions.py

--- spotify/mergeToDohmaen.py
+++ spotify/mergeToDohmaen.py
@@ -1,59 +1,6 @@
 from random import shuffle
 from datetime import date
-import requests
-
-
-def create_playlist(access_token: str, name: str, public: bool = True, description: str = ""):
-    response = requests.post(
-        'https://api.spotify.com/v1/users/cedesc3/playlists',
-        headers={
-            "Authorization": f"Bearer {access_token}"
-        },
-        json={
-            "name": name,
-            "public": public,
-            "description": description
-        }
-    )
-
-    return response.json()
-
-
-def get_playlist_items(access_token: str, playlist_id: str, limit: int = 25):
-    response = requests.get(
-        f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?fields=items(track(id))&limit={limit}",
-        headers={
-            "Authorization": f"Bearer {access_token}"
-        }
-    )
-
-    return response.json()
-
-
-def add_items_to_playlist(access_token: str, playlist_id: str, uris: list[str]):
-    response = requests.post(
-        f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks",
-        headers={
-            "Authorization": f"Bearer {access_token}"
-        },
-        json={
-            "uris": uris
-        }
-    )
-
-    return response.json()
-
-
-def extract_ids(response: dict) -> list[str]:
-
-    result: list[str] = []
-
-    list_of_songs: list = response['items']
-
-    for i in range(len(list_of_songs)):
-        result.append(list_of_songs[i]['track']['id'])
-
-    return result
+import spotifyAPIFunctions as sAPI
 
 
 def arrange_elements(first_list: list[str], second_list: list[str]) -> list[str]:
@@ -67,22 +14,18 @@ def arrange_elements(first_list: list[str], second_list: list[str]) -> list[str]
     return [first_list[i // 2] if i % 2 == 0 else second_list[i // 2] for i in range(length * 2)]
 
 
-def convert_song_ids_to_uris(ids: list[str]) -> list[str]:
-    return [f"spotify:track:{song_id}" for song_id in ids]
-
-
 def merge_playlist_parts(get_playlist_items_token: str, create_playlist_token: str, add_items_to_playlist_token: str,
                          playlist_part_1: str, playlist_part_2: str, number_of_songs: int = 25):
 
     # get songs of the playlists
-    songs_part_1 = get_playlist_items(get_playlist_items_token, playlist_part_1, limit=number_of_songs)
+    songs_part_1 = sAPI.get_playlist_items(get_playlist_items_token, playlist_part_1, limit=number_of_songs)
     print(f"Get songs - part 1: {songs_part_1}")
-    songs_part_2 = get_playlist_items(get_playlist_items_token, playlist_part_2, limit=number_of_songs)
+    songs_part_2 = sAPI.get_playlist_items(get_playlist_items_token, playlist_part_2, limit=number_of_songs)
     print(f"Get songs - part 2: {songs_part_1}")
 
     # convert to the ids
-    ids_part_1: list[str] = extract_ids(songs_part_1)
-    ids_part_2: list[str] = extract_ids(songs_part_2)
+    ids_part_1: list[str] = sAPI.extract_ids(songs_part_1)
+    ids_part_2: list[str] = sAPI.extract_ids(songs_part_2)
 
     # shuffle lists
     shuffle(ids_part_1)
@@ -90,17 +33,17 @@ def merge_playlist_parts(get_playlist_items_token: str, create_playlist_token: s
 
     # merge lists and convert ids to uris
     merged_list: list[str] = arrange_elements(ids_part_1, ids_part_2)
-    song_uris: list[str] = convert_song_ids_to_uris(merged_list)
+    song_uris: list[str] = sAPI.convert_song_ids_to_uris(merged_list)
 
     # create new playlist
     playlist_name: str = f"Dohmän {date.today().strftime('%d.%m.')}"
-    playlist = create_playlist(create_playlist_token, playlist_name, public=True, description="")
+    playlist = sAPI.create_playlist(create_playlist_token, playlist_name, public=True, description="")
     print(f"Create playlist: {playlist}")
     # save playlist id
     playlist_id = playlist['id']
 
     # fill playlist with songs
-    added_elements = add_items_to_playlist(add_items_to_playlist_token, playlist_id, song_uris)
+    added_elements = sAPI.add_items_to_playlist(add_items_to_playlist_token, playlist_id, song_uris)
     print(f"Added elements: {added_elements}")
 
     return added_elements
---
+++ spotify/spotifyAPIFunctions.py
@@ -0,0 +1,58 @@
+import requests
+
+
+def get_playlist_items(access_token: str, playlist_id: str, limit: int = 25):
+    response = requests.get(
+        f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?fields=items(track(id))&limit={limit}",
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+
+    return response.json()
+
+
+def create_playlist(access_token: str, name: str, public: bool = True, description: str = ""):
+    response = requests.post(
+        'https://api.spotify.com/v1/users/cedesc3/playlists',
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        },
+        json={
+            "name": name,
+            "public": public,
+            "description": description
+        }
+    )
+
+    return response.json()
+
+
+def add_items_to_playlist(access_token: str, playlist_id: str, uris: list[str]):
+    response = requests.post(
+        f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks",
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        },
+        json={
+            "uris": uris
+        }
+    )
+
+    return response.json()
+
+
+def extract_ids(response: dict) -> list[str]:
+
+    result: list[str] = []
+
+    list_of_songs: list = response['items']
+
+    for i in range(len(list_of_songs)):
+        result.append(list_of_songs[i]['track']['id'])
+
+    return result
+
+
+def convert_song_ids_to_uris(ids: list[str]) -> list[str]:
+    return [f"spotify:track:{song_id}" for song_id in ids]
