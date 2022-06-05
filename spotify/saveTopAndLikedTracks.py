c27323f2661493c7ff77bc77d538e8d8de180af6
Christian <irgendwas@nix.de>
2022-06-05 14:25:34 +0200

    feat: createLikedTracksSave() finished

--- spotify/saveTopAndLikedTracks.py
+++ spotify/saveTopAndLikedTracks.py
@@ -56,17 +56,25 @@ def createTopTracksSave(get_playlist_items_token: str, create_playlist_token: st
 
 
 def createLikedTracksSave(get_playlist_items_token: str, create_playlist_token: str, add_items_to_playlist_token: str):
-    """Saves the currently liked tracks."""
+    """Saves the currently liked tracks.
+    The maximum value for the 'limit' of 'get_liked_songs()' is 50, but since I have more than 50 liked songs, the
+    function has to be called multiple times."""
 
     # get songs of favorites
-    tracks = sAPI.get_liked_songs(get_playlist_items_token)
-    print(f"Get liked tracks: {tracks}")
-
-    # the maximum value for the 'limit' is 50, but since I have more than 50 liked songs, the function has to be
-    # called multiple times todo
+    # (saved in the list 'track_packages' instead of 'tracks' because of the maximum limit of 50)
+    track_packages = [sAPI.get_liked_songs(get_playlist_items_token, limit=50, offset=0)]
+    # get amount of liked songs
+    total_track_amount: int = sAPI.get_total_track_amount_of_liked_tracks(track_packages[0])
+    # necessary additional calls of get_like_songs()
+    for i in range((total_track_amount-1) // 50):
+        track_packages.append(sAPI.get_liked_songs(get_playlist_items_token, limit=50, offset=(i+1)*50))
+    print(f"Get liked tracks")
 
     # convert to the ids
-    ids: list[str] = sAPI.extract_ids_get_playlist_items(tracks)
+    # (multiple calls of extract_ids_get_playlist_items() are necessary because the tracks aren't all in one variable)
+    ids: list[str] = []
+    for package in track_packages:
+        ids += sAPI.extract_ids_get_playlist_items(package)
 
     # convert ids to uris
     uris: list[str] = sAPI.convert_song_ids_to_uris(ids)
@@ -74,15 +82,21 @@ def createLikedTracksSave(get_playlist_items_token: str, create_playlist_token: 
     # create new playlist (moving it to the right folder isn't possible so far)
     playlist_name: str = f"Liked Tracks {getMonthInGerman(date.today().strftime('%m'))} {date.today().strftime('%y')}"
     playlist = sAPI.create_playlist(create_playlist_token, playlist_name, public=True, description="")
-    print(f"Create playlist: {playlist}")
+    print(f"Create playlist")
     # save playlist id
     playlist_id = playlist['id']
 
     # fill playlist with songs
-    added_elements = sAPI.add_items_to_playlist(add_items_to_playlist_token, playlist_id, uris)
-    print(f"Added elements: {added_elements}")
+    # (multiple calls of add_items_to_playlist() are necessary since only 100 items can be added at once)
+    added_elements_package = []
+    for i in range(((total_track_amount-1) // 100) + 1):
+        lower_bound = 100 * i
+        upper_bound = (100 * i) + 100
+        added_elements_package.append(
+            sAPI.add_items_to_playlist(add_items_to_playlist_token, playlist_id, uris[lower_bound:upper_bound]))
+    print(f"Added elements")
 
-    return added_elements
+    return added_elements_package
 
 
 if __name__ == '__main__':
